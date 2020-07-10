c0c16cfb0415fb4cec8625fa5b73c6efaa219438
Patrick Murphy <psmurphy02@gmail.com>
2020-07-09 21:59:09 -0700

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./DirectoryComponent";
-import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
-
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
   constructor(props) {
@@ -17,24 +17,29 @@ class Main extends Component {
   onCampsiteSelect(campsiteId) {
     this.setState({ selectedCampsite: campsiteId });
   }
-//{selectedCampsite: campsite changed to : campsiteId to reflect change in name when onClick moved from Directory}
+  //{selectedCampsite: campsite changed to : campsiteId to reflect change in name when onClick moved from Directory}
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
-        <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
-
+        <Header />
+        <Directory
+          campsites={this.state.campsites}
+          onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+        <Footer />
         {/* Updated to above lines when onClick moved from Directory.js */}
         {/* <Directory campsites={this.state.campsites} /> */}
         {/* <CampsiteInfo campsite={this.state.selectedCampsite} /> */}
       </div>
     );
-  };
+  }
 }
 
 export default Main;
