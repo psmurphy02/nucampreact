dd5eb7869d071276990a546066a1cd49b5951fe6
Patrick Murphy <psmurphy02@gmail.com>
2020-07-20 22:57:02 -0700

    Redux Thunk

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Loading = () => {
+    return (
+        <div className="col">
+            <i className="fa fa-spinner fa-pulse fa-3x fa-fw test-primary" />
+            <p>Loading...</p>
+        </div>
+    );
+}
